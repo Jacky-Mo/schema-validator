3a9ba634caaddf4ccebfc28e5c118cc26da9b38f
Jacky Mo <>
2018-09-12 00:30:58 -0400

    upgraded babel lib to v7 and introduced rollup to build output

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+    input: 'src/schemaValidator.js',
+    output: [{
+        file: 'dist/index.cjs.js',
+        format: 'cjs'
+    },
+    {
+        file: 'dist/index.esm.js',
+        format: 'esm'
+    }],
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**' // only transpile our source code
+        })
+    ]
+};
